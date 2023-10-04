1bef4f2a640f3cc0f87fe9faf646c8b08d653e68
Dmitrii2503 <102679550+Dmitrii2503@users.noreply.github.com>
2023-10-04 16:00:24 +0300

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,24 @@
+﻿//              0 1 2 3 4 5 6
+int [] array = {1,2,3,4,5,6,7};
+
+int a = array [0]; 
+int b = array [1];
+int c = array [2];
+int d = array [3];
+int e = array [4];
+int f = array [5];
+int g = array [6];
+
+
+Console.WriteLine ("enter the number  "); 
+int number = Convert.ToInt32 (Console.ReadLine()); 
+
+ if (number == f || number == g)  
+ {  
+    Console.Write ("yes");
+ }
+ else 
+ {
+    Console.Write ("No");
+ }
+
