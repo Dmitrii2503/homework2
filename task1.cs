52150e745b07f737799b2fa356967cc7c5581795
Dmitrii2503 <dastashenko@inbox.ru>
2023-10-04 15:56:20 +0300

    files were commited

---
+++ task1.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("enter the number");
+
+int number = Convert.ToInt32 (Console.ReadLine()) ; 
+
+if (number<100 || number >= 1000) 
+{
+Console.WriteLine ("Your value Contains more or less then 3 numbers in value. Please, Try again");
+return;
+} 
+
+else 
+{
+    Console.Write ($"Entered number is '{number}'");
+    int right_number = number / 10 % 10;
+    Console.WriteLine ($"the second number of value is '{right_number}' ");
+};
+
+
+
+
